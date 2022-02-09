7d021f73a2191cc7453c102e02bb409e8af6cf08
jesusmirena <jesusamh16@gmail.com>
2022-02-08 23:00:28 -0500

    Adding budget's form validation

--- src/Components/NewBudget.jsx
+++ src/Components/NewBudget.jsx
@@ -1,9 +1,18 @@
-import React from "react";
+import { useState } from "react";
+import Message from "./Message";
 
 const NewBudget = ({ budget, setBudget }) => {
+  const [message, setMessage] = useState("");
+
+  const handleBudget = (e) => {
+    e.preventDefault();
+    if (!Number(budget) || !Number(budget) < 0) {
+      setMessage("It's not a valid budget");
+    }
+  };
   return (
     <div className="contenedor-presupuesto contenedor sombra">
-      <form className="formulario">
+      <form onSubmit={handleBudget} className="formulario">
         <div className="campo">
           <label htmlFor="">Define budget</label>
           <input
@@ -13,8 +22,10 @@ const NewBudget = ({ budget, setBudget }) => {
             value={budget}
             onChange={(e) => setBudget(e.target.value)}
           />
-          <input type="submit" value="Add" />
         </div>
+        <input type="submit" value="Add" />
+
+        {message && <Message type="error">{message}</Message>}
       </form>
     </div>
   );
