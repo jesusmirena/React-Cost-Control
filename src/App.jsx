a4efc8f68322eaac2d82e2352f00c90fcdc116f2
jesusmirena <jesusamh16@gmail.com>
2022-02-09 00:09:28 -0500

    Adding a button to register a new expense

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import Header from "./Components/Header";
+import newExpenseIcon from "./img/nuevo-gasto.svg";
+
 function App() {
   const [budget, setBudget] = useState(0);
   const [isValidBudget, setIsValidBudget] = useState(false);
@@ -11,6 +13,11 @@ function App() {
         isValidBudget={isValidBudget}
         setIsValidBudget={setIsValidBudget}
       />
+      {isValidBudget && (
+        <div className="nuevo-gasto">
+          <img src={newExpenseIcon} alt="new Expense Icon" />
+        </div>
+      )}
     </div>
   );
 }
