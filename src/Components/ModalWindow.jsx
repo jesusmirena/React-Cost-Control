95c50289877fb59d337ab74f65f95d0deeb00697
jesusmirena <jesusamh16@gmail.com>
2022-02-09 01:31:59 -0500

    Adding the expense form

--- src/Components/ModalWindow.jsx
+++ src/Components/ModalWindow.jsx
@@ -16,6 +16,33 @@ const ModalWindow = ({ setModal, animateModal, setAnimateModal }) => {
       </div>
       <form className={`formulario ${animateModal ? "animar" : "cerrar"}`}>
         <legend>New Expense</legend>
+        <div className="campo">
+          <label htmlFor="name">Expense Name</label>
+          <input id="name" type="text" placeholder="Add the expense name" />
+        </div>
+        <div className="campo">
+          <label htmlFor="amount">Amount</label>
+          <input
+            id="amount"
+            type="number"
+            placeholder="Add the expense amount"
+          />
+        </div>
+
+        <div className="campo">
+          <label htmlFor="category">Category</label>
+          <select id="category">
+            <option value="">--Select a category--</option>
+            <option value="savings">Savings</option>
+            <option value="food">Food</option>
+            <option value="home">Home</option>
+            <option value="miscellaneous">Miscellaneous expenses</option>
+            <option value="entertainment">Entertainment</option>
+            <option value="health">Health</option>
+            <option value="subscriptions">Subscriptions</option>
+          </select>
+        </div>
+        <input type="submit" value="Add expense" />
       </form>
     </div>
   );
