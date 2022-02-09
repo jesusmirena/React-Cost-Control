9c6df4c1c32d774fe753a64f88b441236f59f2c5
jesusmirena <jesusamh16@gmail.com>
2022-02-09 01:44:11 -0500

    Creating the State for expenses

--- src/Components/ModalWindow.jsx
+++ src/Components/ModalWindow.jsx
@@ -1,6 +1,11 @@
+import { useState } from "react";
 import CloseButton from "../img/cerrar.svg";
 
 const ModalWindow = ({ setModal, animateModal, setAnimateModal }) => {
+  const [name, setName] = useState("");
+  const [amount, setAmount] = useState("");
+  const [category, setCategory] = useState("");
+
   const hideModalWindow = () => {
     setAnimateModal(false);
 
@@ -16,22 +21,36 @@ const ModalWindow = ({ setModal, animateModal, setAnimateModal }) => {
       </div>
       <form className={`formulario ${animateModal ? "animar" : "cerrar"}`}>
         <legend>New Expense</legend>
+
         <div className="campo">
           <label htmlFor="name">Expense Name</label>
-          <input id="name" type="text" placeholder="Add the expense name" />
+          <input
+            id="name"
+            type="text"
+            placeholder="Add the expense name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
         </div>
+
         <div className="campo">
           <label htmlFor="amount">Amount</label>
           <input
             id="amount"
             type="number"
             placeholder="Add the expense amount"
+            value={amount}
+            onChange={(e) => setAmount(Number(e.target.value))}
           />
         </div>
 
         <div className="campo">
           <label htmlFor="category">Category</label>
-          <select id="category">
+          <select
+            id="category"
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+          >
             <option value="">--Select a category--</option>
             <option value="savings">Savings</option>
             <option value="food">Food</option>
