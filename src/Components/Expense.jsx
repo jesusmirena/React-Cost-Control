07a646c5b2b42a0548a6a2a7387e047b60500b11
jesusmirena <jesusamh16@gmail.com>
2022-02-23 00:12:52 -0500

    Adding icons to the expense

--- src/Components/Expense.jsx
+++ src/Components/Expense.jsx
@@ -1,10 +1,29 @@
 import { formatDate } from "../helpers";
+import savingsIcon from "../img/icono_ahorro.svg";
+import foodIcon from "../img/icono_comida.svg";
+import homeIcon from "../img/icono_casa.svg";
+import miscellaneousIcon from "../img/icono_gastos.svg";
+import healthIcon from "../img/icono_salud.svg";
+import entertaimentIcon from "../img/icono_ocio.svg";
+import subscriptionsIcon from "../img/icono_suscripciones.svg";
+
+const iconDictionary = {
+  savings: savingsIcon,
+  food: foodIcon,
+  home: homeIcon,
+  miscellaneous: miscellaneousIcon,
+  entertainment: entertaimentIcon,
+  health: healthIcon,
+  subscriptions: subscriptionsIcon,
+};
+
 const Expense = ({ expense }) => {
   const { category, name, amount, id, date } = expense;
 
   return (
     <div className="gasto sombra">
       <div className="contenido-gasto">
+        <img src={iconDictionary[category]} alt="expense icon" />
         <div className="descripcion-gasto">
           <p className="categoria">{category}</p>
           <p className="nombre-gasto">{name}</p>
